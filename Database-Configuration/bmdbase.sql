219ab766e048e2ab24f1ae11e95015e97a0147bb
davidderichs <s57343@beuth-hochschule.de>
2018-01-12 16:54:30 +0100

    Datenbank-Constraint - UNIQUE für TitelDE und TitelEN eingefügt, damit es keine Duplikate geben kann und die Exception-Message entsprechend sinnvolle Infos ausgeben kann.

--- Database-Configuration/bmdbase.sql
+++ Database-Configuration/bmdbase.sql
@@ -1,13 +1,12 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 08, 2018 at 07:00 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Erstellungszeit: 12. Jan 2018 um 16:53
+-- Server-Version: 10.1.25-MariaDB
+-- PHP-Version: 5.6.31
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -20,19 +19,19 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bmdbase`
+-- Datenbank: `bmdbase`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bmdbase`
+-- Tabellenstruktur für Tabelle `bmdbase`
 --
 
 CREATE TABLE `bmdbase` (
   `id` int(11) NOT NULL,
-  `titel` varchar(300) NOT NULL,
-  `title` varchar(300) NOT NULL,
+  `titel` varchar(20) NOT NULL,
+  `title` varchar(20) NOT NULL,
   `regie` varchar(300) DEFAULT NULL,
   `genre` varchar(300) DEFAULT NULL,
   `schauspieler` varchar(300) DEFAULT NULL,
@@ -48,33 +47,34 @@ CREATE TABLE `bmdbase` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `bmdbase`
+-- Daten für Tabelle `bmdbase`
 --
 
 INSERT INTO `bmdbase` (`id`, `titel`, `title`, `regie`, `genre`, `schauspieler`, `produzent`, `studio`, `fsk`, `sprache`, `laenge`, `date`, `land`, `farbe`, `bewertung`) VALUES
-(1, 'Gladiator', 'Gladiator', 'Ridley Scott', 'Drama, Action, Adventure', 'Russel Crowe', NULL, NULL, 18, 'englisch, deutsch', 155, NULL, 'Amerika', NULL, 8);
+(1, 'Gladiator', 'Gladiator', 'Ridley Scott', 'Drama, Action, Adventure', 'Russel Crowe', NULL, NULL, 18, 'englisch, deutsch', 155, '2005-05-25', 'Amerika', NULL, 8),
+(2, 'TESTFilm', 'TESTMovie', NULL, NULL, 'Russel Crowe', NULL, NULL, 18, 'deutsch', 140, '2018-01-01', NULL, NULL, NULL);
 
 --
--- Indexes for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `bmdbase`
+-- Indizes für die Tabelle `bmdbase`
 --
 ALTER TABLE `bmdbase`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `titel` (`titel`),
+  ADD UNIQUE KEY `title` (`title`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `bmdbase`
+-- AUTO_INCREMENT für Tabelle `bmdbase`
 --
 ALTER TABLE `bmdbase`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-SET FOREIGN_KEY_CHECKS=1;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

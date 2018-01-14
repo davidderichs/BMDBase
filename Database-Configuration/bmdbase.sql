c852d4e0adbec5b984b4f343d36f526a6ba67d6a
s57343 <s57343@beuth-hochschule.de>
2018-01-14 16:16:13 +0100

    Mehr Datensätze, Datenbank-Konfigurationverbessert.

--- Database-Configuration/bmdbase.sql
+++ Database-Configuration/bmdbase.sql
@@ -1,12 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 12. Jan 2018 um 16:53
--- Server-Version: 10.1.25-MariaDB
--- PHP-Version: 5.6.31
+-- Generation Time: Jan 14, 2018 at 04:15 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -19,19 +20,21 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `bmdbase`
+-- Database: `bmdbase`
 --
+CREATE DATABASE IF NOT EXISTS `bmdbase` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `bmdbase`;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `bmdbase`
+-- Table structure for table `bmdbase`
 --
 
 CREATE TABLE `bmdbase` (
   `id` int(11) NOT NULL,
-  `titel` varchar(20) NOT NULL,
-  `title` varchar(20) NOT NULL,
+  `titel` varchar(300) NOT NULL,
+  `title` varchar(300) NOT NULL,
   `regie` varchar(300) DEFAULT NULL,
   `genre` varchar(300) DEFAULT NULL,
   `schauspieler` varchar(300) DEFAULT NULL,
@@ -47,34 +50,37 @@ CREATE TABLE `bmdbase` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `bmdbase`
+-- Dumping data for table `bmdbase`
 --
 
 INSERT INTO `bmdbase` (`id`, `titel`, `title`, `regie`, `genre`, `schauspieler`, `produzent`, `studio`, `fsk`, `sprache`, `laenge`, `date`, `land`, `farbe`, `bewertung`) VALUES
-(1, 'Gladiator', 'Gladiator', 'Ridley Scott', 'Drama, Action, Adventure', 'Russel Crowe', NULL, NULL, 18, 'englisch, deutsch', 155, '2005-05-25', 'Amerika', NULL, 8),
-(2, 'TESTFilm', 'TESTMovie', NULL, NULL, 'Russel Crowe', NULL, NULL, 18, 'deutsch', 140, '2018-01-01', NULL, NULL, NULL);
+(1, 'Gladiator', 'Gladiator', 'Ridley Scott', 'Drama, Action, Adventure', 'Russel Crowe', NULL, NULL, 18, 'englisch, deutsch', 155, '2000-05-25', 'Amerika', NULL, 8),
+(5, 'test2', 'test2', NULL, NULL, 'test2darst', NULL, NULL, 18, 'englisch, deutsch', 222, '2000-01-01', NULL, NULL, NULL),
+(9, 'test3', 'test3', NULL, NULL, 'test3darst', NULL, NULL, 18, 'englisch, deutsch', 333, '3000-01-01', NULL, NULL, NULL),
+(10, 'test4', 'test4', NULL, NULL, 'test4darst', NULL, NULL, 18, 'englisch, deutsch', 444, '4000-01-01', NULL, NULL, NULL),
+(13, 'test5', 'test5', NULL, NULL, 'test5darst', NULL, NULL, 18, 'englisch, deutsch', 555, '5000-01-01', NULL, NULL, NULL);
 
 --
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `bmdbase`
+-- Indexes for table `bmdbase`
 --
 ALTER TABLE `bmdbase`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `titel` (`titel`),
-  ADD UNIQUE KEY `title` (`title`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `bmdbase`
+-- AUTO_INCREMENT for table `bmdbase`
 --
 ALTER TABLE `bmdbase`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
